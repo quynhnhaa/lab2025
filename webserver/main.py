0c2928cbbb7ef474ed66f34dd28adf4c9bc54db1
nhaquynh <quynhnhaa@gmail.com>
2025-09-22 10:54:43 +0700

    update led

--- webserver/main.py
+++ webserver/main.py
@@ -6,7 +6,7 @@ import time
 import threading
 
 # --- GPIO setup ---
-LED_PINS = [17, 18, 27, 22]  # 4 LED GPIO pins
+LED_PINS = [14, 15, 27, 22]  # 4 LED GPIO pins
 GPIO.setmode(GPIO.BCM)
 pwms = []
 for pin in LED_PINS:
