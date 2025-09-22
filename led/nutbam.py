38f3598c0640dc272f8feebd135930ddca665a58
nhaquynh <quynhnhaa@gmail.com>
2025-09-22 09:04:48 +0700

    update led

--- led/nutbam.py
+++ led/nutbam.py
@@ -1,36 +1,35 @@
 import lgpio
 import time
 
-# Pin BCM
-BUTTON_PIN = 17
-LED_PIN = 27
+BUTTON_PIN = 17   # BCM pin cho nút
+LED_PIN = 27      # BCM pin cho LED
 
-# Mở chip GPIO
+# Mở chip GPIO (chip số 0)
 chip = lgpio.gpiochip_open(0)
 
-# Cấu hình pin
+# Cấu hình chân
 lgpio.gpio_claim_input(chip, BUTTON_PIN)
 lgpio.gpio_claim_output(chip, LED_PIN)
 
 # Hàm callback khi có ngắt
 def button_callback(chip, gpio, level, tick):
-    print(f"[CALLBACK] Pin {gpio} Level {level} Tick {tick}")
-    if level == 1:  # Nhấn nút
+    print(f"[CALLBACK] GPIO{gpio} Level={level} Tick={tick}")
+    if level == 1:   # Nút nhấn xuống
         lgpio.gpio_write(chip, LED_PIN, 1)
-    elif level == 0:  # Nhả nút
+    elif level == 0: # Nút nhả ra
         lgpio.gpio_write(chip, LED_PIN, 0)
 
 # Chống dội 200ms
 lgpio.gpio_set_debounce_micros(chip, BUTTON_PIN, 200000)
 
-# Đăng ký callback cho rising + falling
-lgpio.gpio_claim_alert(chip, BUTTON_PIN, lgpio.RISING_EDGE | lgpio.FALLING_EDGE)
-lgpio.gpio_set_callbacks(chip, BUTTON_PIN, button_callback)
+# Đăng ký ngắt cho cả 2 cạnh
+lgpio.gpio_claim_alert(chip, BUTTON_PIN, lgpio.BOTH_EDGES)
+lgpio.gpio_set_alert_func(chip, BUTTON_PIN, button_callback)
 
-print("Chờ nút bấm... Ctrl+C để thoát")
+print("Chờ nút bấm... (Ctrl+C để thoát)")
 try:
     while True:
-        time.sleep(1)
+        time.sleep(1)  # Chương trình chính ngủ, callback tự chạy khi có ngắt
 except KeyboardInterrupt:
     print("Thoát...")
     lgpio.gpiochip_close(chip)
